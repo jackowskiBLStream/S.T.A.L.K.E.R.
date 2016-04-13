19cb67ccfa706f6674590891de123ae380304779
jackowskiBLStream <damian.jackowski@blstream.com>
2016-04-13 12:23:03 +0200

    init version

---
+++ app/src/main/java/com/blstream/stalker/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.blstream.stalker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ */
+    public abstract class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
