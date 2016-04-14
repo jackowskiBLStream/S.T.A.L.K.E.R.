31021b3b3c088afc7f82902ea64c870c3a55c89b
mkaczynska <magdalena.kaczynska@blstream.com>
2016-04-14 14:29:18 +0200

    Slight changes Login Controller

--- app/src/main/java/com/blstream/stalker/controller/LoginScreenController.java
+++ app/src/main/java/com/blstream/stalker/controller/LoginScreenController.java
@@ -1,17 +1,16 @@
 package com.blstream.stalker.controller;
 
-
 import android.app.Activity;
-import android.content.Context;
 import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.widget.Toast;
 
 import com.blstream.stalker.view.fragments.LoginScreenFragment;
 import com.blstream.stalker.view.interfaces.IMainFragment;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.plus.Plus;
 
@@ -19,24 +18,16 @@ public class LoginScreenController implements GoogleApiClient.ConnectionCallback
 
     public static final int RC_SIGN_IN = 0;
     LoginScreenFragment fragment;
-    Context context;
+    Activity activity;
     private GoogleApiClient mGoogleApiClient;
     private boolean signedInUser;
     private boolean mIntentInProgress;
     private ConnectionResult mConnectionResult;
 
-    public LoginScreenController(Fragment fragment) {
+    private LoginScreenController(Fragment fragment) {
         this.fragment = (LoginScreenFragment) fragment;
-        context = fragment.getActivity();
-        initalizeGPApiClient();
-    }
-
-    private void initalizeGPApiClient() {
-        // Initializing google plus api client
-        mGoogleApiClient = new GoogleApiClient.Builder(context.getApplicationContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this).addApi(Plus.API)
-                .addScope(Plus.SCOPE_PLUS_LOGIN).build();
+        activity = fragment.getActivity();
+        initializeGPApiClient();
     }
 
     public void googlePlusLogin() {
@@ -44,6 +35,26 @@ public class LoginScreenController implements GoogleApiClient.ConnectionCallback
             signedInUser = true;
             resolveSignInError();
         }
+    }
+
+    public void sentLoginResultToController(int requestCode, int responseCode, final int RESULT_OK) {
+        if (requestCode == RC_SIGN_IN) {
+            if (responseCode == RESULT_OK) {
+                signedInUser = false;
+            }
+            mIntentInProgress = false;
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    private void initializeGPApiClient() {
+        // Initializing google plus api client
+        mGoogleApiClient = new GoogleApiClient.Builder(activity.getApplicationContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN).build();
     }
 
     /**
@@ -55,7 +66,7 @@ public class LoginScreenController implements GoogleApiClient.ConnectionCallback
             if (resolution) {
                 mIntentInProgress = true;
                 mConnectionResult.startResolutionForResult(fragment.getActivity(), RC_SIGN_IN);
-                Toast.makeText(context, "startResolutionForResult", Toast.LENGTH_SHORT).show();
+                Toast.makeText(activity, "startResolutionForResult", Toast.LENGTH_SHORT).show();
 
             }
         } catch (IntentSender.SendIntentException | NullPointerException e) {
@@ -65,10 +76,14 @@ public class LoginScreenController implements GoogleApiClient.ConnectionCallback
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        if (!result.hasResolution()) {
-            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), (Activity) context,
-                    0).show();
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        boolean resolution = mConnectionResult.hasResolution();
+        if (!resolution) {
+            GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+            int code = googleApiAvailability.isGooglePlayServicesAvailable(activity);
+            if (googleApiAvailability.isUserResolvableError(code)) {
+                googleApiAvailability.getErrorDialog(activity, code, RC_SIGN_IN).show();
+            }
             return;
         }
 
@@ -87,25 +102,13 @@ public class LoginScreenController implements GoogleApiClient.ConnectionCallback
 
     @Override
     public void onConnected(Bundle arg0) {
-        Toast.makeText(context, "OnConnected", Toast.LENGTH_SHORT).show();
+        Toast.makeText(activity, "OnConnected", Toast.LENGTH_SHORT).show();
         fragment.changeFragment(IMainFragment.LIST_FRAGMENT);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Toast.makeText(context, "OnConnectedSuspended", Toast.LENGTH_SHORT).show();
+        Toast.makeText(activity, "OnConnectedSuspended", Toast.LENGTH_SHORT).show();
         mGoogleApiClient.connect();
-    }
-
-    public void sentLoginResultToController(int requestCode, int responseCode, final int RESULT_OK) {
-        if (requestCode == RC_SIGN_IN) {
-            if (responseCode == RESULT_OK) {
-                signedInUser = false;
-            }
-            mIntentInProgress = false;
-            if (!mGoogleApiClient.isConnecting()) {
-                mGoogleApiClient.connect();
-            }
-        }
     }
 }
