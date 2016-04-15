2e2a56e3d279fff19811ec272e8795edc4150f57
Patryk <pgwiazdowski@gmail.com>
2016-04-15 13:27:24 +0200

    implemented more database

--- app/src/main/java/com/blstream/stalker/controller/DatabaseController.java
+++ app/src/main/java/com/blstream/stalker/controller/DatabaseController.java
@@ -2,17 +2,23 @@ package com.blstream.stalker.controller;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 
 import com.blstream.stalker.controller.database.DatabaseHelper;
 import com.blstream.stalker.controller.database.PlacesContentProvider;
 import com.blstream.stalker.controller.database.TableDetails;
 import com.blstream.stalker.controller.database.TablePlaces;
+import com.blstream.stalker.controller.database.TableReviews;
 import com.blstream.stalker.controller.interfaces.IDatabaseController;
 import com.blstream.stalker.model.PlaceData;
+import com.blstream.stalker.model.PlaceDataDetails;
 import com.blstream.stalker.model.PlaceDataWithDetails;
+import com.blstream.stalker.model.Review;
+import com.blstream.stalker.model.interfaces.IReviews;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -33,6 +39,7 @@ public class DatabaseController implements IDatabaseController {
      */
     @Override
     public void clearDB() {
+
     }
 
     /**
@@ -60,19 +67,59 @@ public class DatabaseController implements IDatabaseController {
     }
 
     private boolean addPlace(PlaceDataWithDetails data) {
-        PlaceData placeData = data.getPlaceData();
+
+        int placeDataId = addPlaceData(data.getPlaceData());
+        Log.d(TAG, "addPlace: Added row: " + placeDataId);
+        if (placeDataId == -1){
+            return false;
+        }
+        addDataDetails(data.getPlaceDataDetails(),placeDataId);
+        return true;
+    }
+
+    /**
+     *
+     * @param placeData Data to add
+     * @return row id
+     */
+    private int addPlaceData(PlaceData placeData){
         ContentValues valuesData = new ContentValues();
         valuesData.put(TablePlaces.COLUMN_NAME, placeData.getName());
         valuesData.put(TablePlaces.COLUMN_IMG_URL, placeData.getIconUrl());
         valuesData.put(TablePlaces.COLUMN_TYPES, placeData.getTypes());
         valuesData.put(TablePlaces.COLUMN_LATITUDE, placeData.getLocation().getLatitude());
         valuesData.put(TablePlaces.COLUMN_LONGITUDE,placeData.getLocation().getLongitude());
-        
-        Log.d(TAG, "addPlace: "+context.getContentResolver().insert(PlacesContentProvider.URI_PLACES, valuesData));
+        Uri uri = context.getContentResolver().insert(PlacesContentProvider.URI_PLACES, valuesData);
+        String rowNumber;
+        if (uri != null) {
+            rowNumber = uri.getLastPathSegment();
+            return Integer.valueOf(rowNumber);
+        }
+        return -1;
 
-        ContentValues valuesDetails = new ContentValues();
-        valuesDetails.put(TableDetails.COLUMN_RATING, )
+    }
 
-        return true;
+    private void addDataDetails(PlaceDataDetails placeDataDetails, int placeDataId) {
+        ContentValues values = new ContentValues();
+        values.put(TableDetails.COLUMN_RATING, placeDataDetails.getRating());
+        values.put(TableDetails.COLUMN_PLACE_ID, placeDataId);
+        for (int i=0; i<7; i++){
+            values.put(TableDetails.COLUMN_OPEN_DAY[i], placeDataDetails.getOpenHours(i).getCloseTime());
+            values.put(TableDetails.COLUMN_CLOSE_DAY[i], placeDataDetails.getOpenHours(i).getCloseTime());
+        }
+        context.getContentResolver().insert(PlacesContentProvider.URI_DETAILS, values);
+        addDataReviews(placeDataDetails.getReviews(), placeDataId);
+    }
+
+    private void addDataReviews(Collection<IReviews> reviews, int placeDataId) {
+        ContentValues values;
+        for (IReviews review : reviews){
+            values = new ContentValues();
+            values.put(TableReviews.COLUMN_REVIEW, review.getReview());
+            values.put(TableReviews.COLUMN_AUTHOR, review.getAuthor());
+            values.put(TableReviews.COLUMN_RATING, review.getRating());
+            values.put(TableReviews.COLUMN_PLACE_ID, placeDataId);
+            context.getContentResolver().insert(PlacesContentProvider.URI_REVIEWS, values);
+        }
     }
 }
